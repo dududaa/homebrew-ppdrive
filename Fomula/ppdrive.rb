34d5b53316f9617bd0ba13eb2e0bf83c7026ea48
Ajayi Olamide <prodbyola@gmail.com>
2025-10-21 15:34:28 -0700

    added ppdrive formula v0.1.0-rc.1

---
+++ Fomula/ppdrive.rb
@@ -0,0 +1,20 @@
+class Ppdrive < Formula
+  desc "PPDRIVE — Powerful parallel data transfer and management CLI"
+  homepage "https://github.com/dududaa/ppdrive"
+  version "v0.1.0-rc.1"
+  license "MIT"
+
+  on_macos do
+    url "https://github.com/dududaa/ppdrive/releases/download/v0.1.0-rc.1/ppdrive-macos.tar.gz"
+    sha256 "b95305564ed0053918eaeace163a3c0b7abb97e941b47f5fa3338395c3201ba7"
+  end
+
+  def install
+    bin.install "ppdrive"
+    bin.install "manager"
+  end
+
+  test do
+    system "#{bin}/ppdrive", "--help"
+  end
+end
